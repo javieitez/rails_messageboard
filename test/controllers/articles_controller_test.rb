bafcf9f0800474b894effe9ab2842babc4130d0e
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-15 13:49:03 +0000

    Basic tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @base_title = "Message Board"
+  end
+
+  
+  test "should get index" do
+    get articles_path
+    assert_response :success
+    assert_select "title", "All messages | #{@base_title}"
+  end
+  
+  test "should get new" do
+    get new_article_path
+    assert_response :success
+    assert_select "title", "Compose new message | #{@base_title}"
+  end
+
 end
