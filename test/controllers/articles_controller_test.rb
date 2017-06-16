4d1479851a28a83ca2fb0f4e21f744482d718001
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-16 12:31:50 +0000

    articles html tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,61 +4,45 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
       @base_title = "Message Board"
-      @invalid_subject = "1234"
-      @invalid_text = "123456789_123456789_1234"
-      @valid_subject = "a string longer than 5 characters"
-      @valid_text = "a string longer than twenty five characters"
   end
 
-  test "should get root" do # root must be articles index
+  test "should get root + title" do # root must be articles index
     get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
-  test "should get index" do
+  test "should get index + title" do
     get articles_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
   
-  test "should get new" do
+  test "should get new + title" do
     get new_article_path
     assert_response :success
     assert_select "title", "Compose new message | #{@base_title}"
   end
 
-  test "should save valid note" do
-    article= Article.new
-    article.subject = @valid_subject
-    article.text = @valid_text
-    assert article.save, "Cannot save a valid note"
-  end
-  
-  test "should not save empty note" do
-    article = Article.new
-    assert_not article.save, "Saved an empty note"
-  end
-
-  test "should not save note with short subject" do
-    article = Article.new
-    article.subject = @invalid_subject
-    article.text = @valid_text
-    assert_not article.save, "Saved note with short subject"
+  test "should redirect and flash when posting new note" do
+    get new_article_path
+    post articles_path, params: {article:  { subject: "123456", 
+                                                  text: "a" * 100 } } 
+    assert_response :redirect, "Not redirected"
+    follow_redirect!
+    assert_response :success
+    assert_not flash.empty?, "Flash is empty"
+    assert_select 'th', '123456', "note not rendered"
   end
 
-  test "should not save note with subject too long" do
-    article = Article.new
-    article.subject = @valid_subject * 3
-    article.text = @valid_text
-    assert_not article.save, "Saved note with subject too long"
-  end
-
-  test "should not save note with short content" do
-    article = Article.new
-    article.subject = @valid_subject
-    article.text = @invalid_text
-    assert_not article.save, "Saved note with short content"
+  test "should raise error when posting incorrect note" do
+    get new_article_path
+    post articles_path, params: {article:  { subject: "123", 
+                                                  text: "a" * 100 } } 
+    assert_select 'th', {count: 0, text: "123"}, "note must not be
+                                                              rendered in table"
+    assert_select 'input[value=?]', '123'
+    assert_select 'div[id=?]', 'error_explanation'
   end
 
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+      @invalid_subject = "1234"
+      @invalid_text = "123456789_123456789_1234"
+      @valid_subject = "a string longer than 5 characters"
+      @valid_text = "a string longer than twenty five characters"
+  end
+
+  test "should save valid note" do
+    article= Article.new
+    article.subject = @valid_subject
+    article.text = @valid_text
+    assert article.save, "Cannot save a valid note"
+  end
+  
+  test "should not save empty note" do
+    article = Article.new
+    assert_not article.save, "Saved an empty note"
+  end
+
+  test "should not save note with short subject" do
+    article = Article.new
+    article.subject = @invalid_subject
+    article.text = @valid_text
+    assert_not article.save, "Saved note with short subject"
+  end
+
+  test "should not save note with subject too long" do
+    article = Article.new
+    article.subject = @valid_subject * 3
+    article.text = @valid_text
+    assert_not article.save, "Saved note with subject too long"
+  end
+
+  test "should not save note with short content" do
+    article = Article.new
+    article.subject = @valid_subject
+    article.text = @invalid_text
+    assert_not article.save, "Saved note with short content"
+  end
+
 end
