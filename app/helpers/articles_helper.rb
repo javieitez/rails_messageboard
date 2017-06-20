2ac63a636cef68b040bfcf2001689357a816bf6a
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-20 11:36:56 +0000

    finished sanitizing input

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,18 @@
 module ArticlesHelper
+  
+  #searches for insanely long words
+    def contains_insanely_long_words(note)
+      @status = 0 
+      note.split.each do |s|
+        if s.length > 45 # longest word in english dictionary
+          @status += 1
+        end
+      end
+        if @status == 0 
+          return false
+        else
+          return true
+        end
+    end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
      validates :subject, presence: true,
-                        length: { minimum: 5, maximum: 90 }
+                        length: { minimum: 5, maximum: 90 },
+                         format: { without: /((.)\2{7,})/, 
+                         message: "contains too many repeated characters" }
      validates :text, presence: true,
                          length: {minimum: 25}
 end
