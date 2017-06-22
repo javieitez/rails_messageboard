d0801ab96ab75ff7c32040af2bc86977e7fd36a4
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-22 13:39:08 +0000

    steroid improved tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -28,31 +28,49 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get new_article_path
     assert_response :success
-    assert_select "title", "Compose new message | #{@base_title}"
+    assert_select "title", "Compose new Note | #{@base_title}"
     assert_select 'input[type=file]'
   end
 
   test "post new note with image, then edit" do
+  #init vars
     picture = fixture_file_upload('kitten.jpg', 'image/jpg')
-    get new_article_path
-    post articles_path, params: {article:  { subject: "123456", 
-                                                  text: "a " * 100,
-                                                  picture: picture} } 
-    assert_response :redirect, "Not redirected"
-    follow_redirect!
-    assert_response :success
-    assert_not flash.empty?, "no Flash on new note"
-    assert_select 'th', '123456', "note not rendered"
-    assert_select 'img[alt=?]', 'Kitten'
-    assert_select 'a[href=?]', edit_article_path
-    get edit_article_path
-    put article_path, params: {article: {subject: "subject changed", 
-                                            remove_picture: true }}
-    assert_response :redirect, "Not redirected"
-    follow_redirect!
-    assert_not flash.empty?, "no Flash on edited note"
-    assert_select 'th', 'subject changed'
-    assert_select 'img[alt=?]', 'Kitten', count: 0
+    picture2 = fixture_file_upload('rails.png', 'image/png')
+    #create a new note 
+      get new_article_path
+      post articles_path, params: {article:  { subject: "123456", 
+                                                    text: "a " * 100,
+                                                    picture: picture} } 
+  #check the note has been saved and
+  #that things are in place
+      assert_response :redirect, "Not redirected"
+      follow_redirect!
+      assert_response :success
+      assert_not flash.empty?, "no Flash on new note"
+      assert_select 'th', '123456'
+      assert_select 'img[alt=?]', 'Kitten'
+      assert_select 'a[href=?]', edit_article_path
+  #edit the note, just replacing the picture
+      get edit_article_path
+      assert_select "title", "Edit Note | #{@base_title}"
+      put article_path, params: {article: {picture: picture2}}
+  #check for the picture to be replaced 
+  #and the subject to remain the same
+      assert_response :redirect, "Not redirected"
+      follow_redirect!
+      assert_response :success
+      assert_select 'img[alt=?]', 'Rails'
+      assert_select 'th', '123456'    
+  #now edit the subject and remove the picture 
+      get edit_article_path
+      put article_path, params: {article: {subject: "subject changed", 
+                                              remove_picture: true }}
+  #check for the changed subject and the removed image
+      assert_response :redirect, "Not redirected"
+      follow_redirect!
+      assert_not flash.empty?, "no Flash on edited note"
+      assert_select 'th', 'subject changed'
+      assert_select 'img[alt=?]', 'Kitten', count: 0
   end
 
   test "should raise error when posting incorrect note" do
