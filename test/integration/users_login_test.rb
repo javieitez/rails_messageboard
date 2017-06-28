6e79845154456eb193a9da565ceafd7097c38dfc
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-28 14:26:14 +0000

    login and logaout implemented

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
-  test "login with invalid information" do
+  def setup
+    @user = users(:user1)
+  end
+
+  
+  test "invalid login should return only 1 flash" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -11,5 +16,32 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information, then logout" do
+    get login_path
+    assert_select "a[href=?]", login_path, count: 0
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", signup_path, count: 0
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user), count: 0
+    #check if the profile link is in the home page
+      get root_path
+      assert_select "a[href=?]", user_path(@user)
+    #logout
+      delete logout_path
+      assert_not is_logged_in?
+      assert_redirected_to root_url
+      follow_redirect!
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", logout_path,      count: 0
+      assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "Everything wrong in signup form" do
     get signup_path
     assert_select "title", "Create your account | #{@base_title}"
+    assert_select "a[href=?]", signup_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                         email: "user@invalid",
@@ -61,6 +63,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
   end
 
-
+  test "valid signup and autologin" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "user name",
+                                        email: "user@val.id",
+                                        username: "username",
+                                        password:              "foo123456789",
+                                        password_confirmation: "foo123456789" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 
 end
