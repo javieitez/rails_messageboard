4c28479af825e9628db21de6c17ccf53aeaa6ac7
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-28 11:09:11 +0000

    form signup tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,20 +6,61 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       @base_title = "Message Board"
   end
 
-  test "invalid signup information" do
+  test "Everything wrong in signup form" do
     get new_user_path
     assert_select "title", "Create your account | #{@base_title}"
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+                                        email: "user@invalid",
+                                        username: "user name",
+                                        password:              "foo",
+                                        password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
-
   end
+
+  test "invalid password confirmation in form" do
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "user name",
+                                        email: "user@val.id",
+                                        username: "username",
+                                        password:              "foo123456789",
+                                        password_confirmation: "bar123456789" } }
+    end
+  end
+
+  test "password too short in form" do
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "user name",
+                                        email: "user@val.id",
+                                        username: "username",
+                                        password:              "foo",
+                                        password_confirmation: "foo" } }
+    end
+  end
+
+  test "invalid username in form" do
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "user name",
+                                        email: "user@val.id",
+                                        username: "user name",
+                                        password:              "foo123456789",
+                                        password_confirmation: "foo123456789" } }
+    end
+  end
+
+  test "invalid email in form" do
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "user name",
+                                        email: "user@invalid",
+                                        username: "user name",
+                                        password:              "foo123456789",
+                                        password_confirmation: "foo123456789" } }
+    end
+  end
+
 
 
 end
