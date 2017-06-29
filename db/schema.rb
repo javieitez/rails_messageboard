b0f44de20a569c10f7a9a1804ed51d40810818fc
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-29 11:13:10 +0000

    completed login cookie and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627115310) do
+ActiveRecord::Schema.define(version: 20170629090534) do
 
   create_table "articles", force: :cascade do |t|
     t.string "subject"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20170627115310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
