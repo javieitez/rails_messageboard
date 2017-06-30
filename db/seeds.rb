1522cc53e1fdf0633af996ed846883662c99d2f5
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-30 12:28:54 +0000

    index users finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "El Usuarito",
+             username:"elusuarito",
+             email: "example@railstutorial.org",
+             password:              "123456789",
+             password_confirmation: "123456789")
+
+User.create!(name:  "El JA",
+             username:"elja",
+             email: "javieitez@grupocmc.es",
+             password:              "123456789",
+             password_confirmation: "123456789")
+
+
+
+99.times do |n|
+  name  = Faker::Name.name
+  username = name.downcase.tr('^A-Za-z0-9', '')
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               username: username,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
