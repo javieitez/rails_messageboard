915c62843715e4301d23f40e0d2425d45631741b
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-06-30 07:25:24 +0000

    first tdd test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   
   def index
   end
@@ -44,5 +45,13 @@ class UsersController < ApplicationController
             params.require(:user).permit(:name, :username, :email, :password,
                                          :about, :password_confirmation)
         end
-        
+
+        # Confirms a logged-in user.
+        def logged_in_user
+          unless logged_in?
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+          end
+        end
+
 end
