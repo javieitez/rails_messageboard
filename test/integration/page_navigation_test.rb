d6a7060c294ec3ecf54a685333eaf2a36902eafe
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-07-07 12:03:00 +0000

    tests refactored + navbar link tests

--- test/integration/page_navigation_test.rb
+++ test/integration/page_navigation_test.rb
@@ -24,5 +24,44 @@ class PageNavigationTest < ActionDispatch::IntegrationTest
     assert_select 'input[type=file]'
   end
 
+  test "navigation links change on context" do
+    get root_path
+      assert_select 'a[href=?]', login_path
+      assert_select 'a[href=?]', logout_path, count: 0
+      assert_select 'a[href=?]', signup_path
+      assert_select 'a[href=?]', users_path, count: 0
+      assert_select 'a[href=?]', user_path(@user), count: 0
+      assert_select 'a[href=?]', edit_user_path(@user), count: 0
+    log_in_as @user
+    get root_path
+      assert_select 'a[href=?]', login_path, count: 0
+      assert_select 'a[href=?]', logout_path
+      assert_select 'a[href=?]', signup_path, count: 0
+      assert_select 'a[href=?]', users_path
+      assert_select 'a[href=?]', user_path(@user)
+      assert_select 'a[href=?]', edit_user_path(@user) 
+    get users_path
+      assert_select 'a[href=?]', login_path, count: 0
+      assert_select 'a[href=?]', logout_path
+      assert_select 'a[href=?]', signup_path, count: 0
+      assert_select 'a[href=?]', users_path, count: 0
+      assert_select 'a[href=?]', user_path(@user)
+      assert_select 'a[href=?]', edit_user_path(@user) 
+    get user_path(@user)
+      assert_select 'a[href=?]', login_path, count: 0
+      assert_select 'a[href=?]', logout_path
+      assert_select 'a[href=?]', signup_path, count: 0
+      assert_select 'a[href=?]', users_path
+      assert_select 'a[href=?]', user_path(@user), count: 0
+      assert_select 'a[href=?]', edit_user_path(@user) 
+    get edit_user_path(@user)
+      assert_select 'a[href=?]', login_path, count: 0
+      assert_select 'a[href=?]', logout_path
+      assert_select 'a[href=?]', signup_path, count: 0
+      assert_select 'a[href=?]', users_path
+      assert_select 'a[href=?]', user_path(@user)
+      assert_select 'a[href=?]', edit_user_path(@user), count: 0
+  end
+
 
 end
